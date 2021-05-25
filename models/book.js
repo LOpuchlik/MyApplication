e1148df92b9bf307c824ec8a2245b15fae9f298a
LOpuchlik <lidia.opuchlik@gmail.com>
2021-05-25 14:12:56 +0200

    Added book model to models, bookRouter and use directive in server.js and prepared empty routes templates for books

---
+++ models/book.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+// Create a schema object
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    publishDate: {
+        type: Date,
+        required: true
+    },
+    pageCount: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    coverImageName: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Author'
+    },
+})
+// this is a table in my database
+module.exports = mongoose.model('Book', bookSchema)
---
+++ routes/books.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+//importing author
+const Book = require('../models/book')
+
+// All books route
+router.get('/', async (req, res) => {
+
+})
+
+// New book route
+router.get('/new', (req, res) => {
+   
+})
+
+// Create book route
+router.post('/', async (req, res) => {
+
+})
+
+module.exports = router
